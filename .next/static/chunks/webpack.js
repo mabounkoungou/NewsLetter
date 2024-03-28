0c2b1e5f9a96209ddd5ffbdb62d564861d2a32de
Raad <mabounkoungou001@st.ug.edu.gh>
2024-03-28 16:02:23 +0000

    Creating all neccessary files and folders for the application

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9e5bab7fad8d36b8"; }
+/******/ 		__webpack_require__.h = function() { return "f86d0b394a8d9413"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
