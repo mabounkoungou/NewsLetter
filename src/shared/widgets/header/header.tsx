8995be9ec2c01f92b96773cfef3d324c58efc3e1
Ghostwriter <mabounkoungou001@st.ug.edu.gh>
2024-04-09 21:22:31 +0000

    Update header.tsx

--- src/shared/widgets/header/header.tsx
+++ src/shared/widgets/header/header.tsx
@@ -8,6 +8,9 @@ const Header = () => {
   return (
     <header className='w-full sticky top-0 left-0 z-[999] border-b border-b[#000] px-10 flex items-center justify-between h-[80px] bg-white text-black'>
 
+
+
+      
         <div>
             <Link href={"/"}>
                 <Logo />
